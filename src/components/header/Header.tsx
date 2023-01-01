6ff16fe22812d2e38006941e6dfcabf85cac9128
anita-brown <efeaniita@gmail.com>
2023-01-01 17:39:52 +0100

    chore: fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import PokemonForm from "../form/PokemonForm";
 
 const Header = () => {
@@ -7,7 +8,9 @@ const Header = () => {
   return (
     <>
       <div className="flex justify-between mb-8">
-        <h1 className="text-orange-700 text-2xl font-bold">Pokemon App</h1>
+        <Link to="/">
+        <h1 className="text-orange-700 text-2xl font-bold cursor-pointer">Pokemon App</h1>
+        </Link>
         <button onClick={() => setShowModal(true)} type="button" className="bg-primary text-white rounded-md px-4 py-2">
           Add Pokemon
         </button>
