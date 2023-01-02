6659c87a8e937b861c2804555ade6efed731d19c
anita-brown <efeaniita@gmail.com>
2023-01-02 09:38:27 +0100

    refix:

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="p-4 bg-white fixed bottom-0 shadow flex md:items-center lg:items-center items-center justify-center md:justify-between md:p-6 lg:w-full md:w-full lg:justify-center md:justify-center ">
+    <footer className="p-4 bg-white fixed bottom-0 shadow flex md:items-center lg:items-center items-center justify-center md:p-6 w-full lg:w-full md:w-full lg:justify-center md:justify-center ">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022{" "}
         <Link to="/" className="hover:underline">
@@ -11,7 +11,6 @@ const Footer = () => {
         </Link>
         . All Rights Reserved.
       </span>
-    
     </footer>
   );
 }
